3c8f97bd9789fa07f27e2e1110a401dad597f178
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-08-01 02:01:42 +0300

    combined reducers & readme

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-module.exports = function reduxBoiler(reducers) {
+function resolveReducerResult(reducer, data, action) {
+	var result = reducer(data, action);
+	if ("object"===typeof result) {
+		for (var prop in result) {
+			if (result.hasOwnProperty(prop)) {
+				if ("function"===typeof result[prop]) result[prop] = result[prop](data[prop], action);
+			}
+		}
+	}
+	return result;
+}
+
+module.exports = function reduxReagent(reducers) {
 	return function(data, resolve, reject) {
 		var getState = this.read.bind(this);
 		var dispatch = function(action) {
 			if ("function"===typeof action) {
 				action(dispatch, getState);
 			} else {
-				resolve(reducers(data, action));
+				
+				resolve(resolveReducerResult(reducers, data, action));
 			}
 		}
 		resolve(data);
